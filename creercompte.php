c6bd207213a384cf588d4edc15176a5c8b1241df
Aymen Kadri <aymenkadri798@gmail.com>
2022-11-03 22:36:59 +0100

    empeche d'avoir plusieurs compte avec le meme mail

--- creercompte.php
+++ creercompte.php
@@ -1,18 +1,25 @@
 <?php
 require_once("header.php");
-if(isset($_POST['prenom']) && strlen($_POST["prenom"]) > 4 && isset($_POST['nom']) && strlen($_POST["nom"]) > 4 && isset($_POST['mdp']) && strlen($_POST["mdp"]) > 5 && isset($_POST['username']) && strlen($_POST["username"]) > 4) {
-    $insertuser = $bdd->prepare("INSERT INTO utilisateurs(prenom,nom,mail,mot_de_passe,username)
-                                VALUES(:prenom,:nom,:mail,:mot_de_passe,:username)");
-    $insertuser->execute([
-        "prenom" => $_POST["prenom"],
-        "nom" => $_POST["nom"],
-        "mail" => $_POST["email"],
-        "mot_de_passe" => md5($_POST["mdp"]),
-        "username" => $_POST["username"]
-    ]);
 
-    header('Location:index.php');
-    die;
+if(isset($_POST['prenom']) && strlen($_POST["prenom"]) > 4 && isset($_POST['nom']) && strlen($_POST["nom"]) > 4 && isset($_POST['mdp']) && strlen($_POST["mdp"]) > 5 && isset($_POST['username']) && strlen($_POST["username"]) > 4) {
+    $email = $_POST['email'];
+    $checkemail = $bdd->prepare("SELECT * FROM utilisateurs WHERE mail=?");
+    $checkemail->execute([$email]); 
+    $lemail = $checkemail->fetch();
+    if(!$lemail){
+        $insertuser = $bdd->prepare("INSERT INTO utilisateurs(prenom,nom,mail,mot_de_passe,username)
+                                    VALUES(:prenom,:nom,:mail,:mot_de_passe,:username)");
+        $insertuser->execute([
+            "prenom" => $_POST["prenom"],
+            "nom" => $_POST["nom"],
+            "mail" => $_POST["email"],
+            "mot_de_passe" => md5($_POST["mdp"]),
+            "username" => $_POST["username"]
+        ]);
+
+        header('Location:index.php');
+        die;
+    }
 }
 
 ?>
@@ -25,16 +32,16 @@ if(isset($_POST['prenom']) && strlen($_POST["prenom"]) > 4 && isset($_POST['nom'
 
             <div class="mb-md-5 mt-md-4 pb-5">
               <h2 class="fw-bold mb-2 text-uppercase">Binamax</h2>
-              <p class="text-white-50 mb-5">Entrer vos information pour créer un compte !</p>
+              <p class="text-white-50 mb-5">Entrer vos informations pour créer un compte !</p>
                 <form action="" method="POST">
                     <div class="form-outline form-white mb-4">
                         <input type="texte" name="prenom" class="form-control form-control-lg" placeholder="Prenom" />
                     </div>
                     <div class="form-outline form-white mb-4">
-                        <input type="texte" name="username" class="form-control form-control-lg" placeholder="Nom d'utilisateur" />
+                        <input type="texte" name="nom" class="form-control form-control-lg" placeholder="Nom" />
                     </div>
                     <div class="form-outline form-white mb-4">
-                        <input type="texte" name="nom" class="form-control form-control-lg" placeholder="Nom" />
+                        <input type="texte" name="username" class="form-control form-control-lg" placeholder="Nom d'utilisateur" />
                     </div>
                     <div class="form-outline form-white mb-4">
                         <input type="email" name="email" class="form-control form-control-lg" placeholder="adresse mail" />
