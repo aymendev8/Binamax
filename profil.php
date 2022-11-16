9ddbcc43d3b59142ebfbfdaac98c3704bc4987ba
Aymen Kadri <aymenkadri798@gmail.com>
2022-11-16 19:50:04 +0100

    modification du profil

--- profil.php
+++ profil.php
@@ -1,42 +1,53 @@
 <?php
 require_once("header.php");
-//require_once("navbar.php");
+if (!isset($_SESSION["id"])) {
+    header("Location:sedeco.php");
+}
+if (isset($_POST['username']) && isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['mail'])) {
+    $heroUpdate = $bdd->prepare('UPDATE utilisateurs SET username = :username, prenom = :prenom, nom = :nom, mail = :mail where id = :id');
+    $heroUpdate->execute([
+        "username" => $_POST['username'],
+        "prenom" => $_POST['prenom'],
+        "nom" => $_POST['nom'],
+        "mail" => $_POST['mail'],
+        "id" => $_SESSION['id']
+    ]);
+    $checkuser = $bdd->prepare("SELECT id, mot_de_passe,administrateur,nom,prenom,username,mail FROM utilisateurs WHERE username=?");
+    $checkuser->execute([$_POST['username']]);
+    $user = $checkuser->fetch();
+        $_SESSION["nom"] = $user["nom"];
+        $_SESSION["username"] = $user["username"];
+        $_SESSION["mail"] = $user["mail"];
+        $_SESSION["prenom"] = $user["prenom"];
+}
+
+if (isset($_POST["argent"])){
+    $heroUpdate = $bdd->prepare('UPDATE utilisateurs SET argent = argent + :argent where id = :id');
+    $heroUpdate->execute([
+        "argent" => $_POST['argent'],
+        "id" => $_SESSION['id']
+    ]);
+    $checkuser = $bdd->prepare("SELECT argent FROM utilisateurs WHERE username=?");
+    $checkuser->execute([$_SESSION['username']]);
+    $user = $checkuser->fetch();
+        $_SESSION["argent"] = $user["argent"];
+}
+require_once("navbar.php");
 ?>
-<h2 id = "modifier_profil">Modifier le profil </h2>
+<link rel="stylesheet" href="Styles/edit.css">
 <br>
-<form action="" method="POST">
-    <div class="row">
-        <div class="col-md-2">
-            <label for="">Username</label>
+            <section id ="edit">
+            <div class="container">
+                <div class="title">
+                    <h2>Modifier le profil</h2>
+                </div>
+                <form action="#" method="POST">
+                    <input value="<?= $_SESSION["username"] ?>" type="text" name="username" placeholder="Votre username" required>
+                    <input value="<?= $_SESSION["nom"] ?>" type="text" name="nom" placeholder="Votre nom" required>
+                    <input value="<?= $_SESSION["prenom"] ?>" type="text" name="prenom" placeholder="Votre prenom" required>
+                    <input value="<?= $_SESSION["mail"] ?>" type="mail" name="mail" placeholder="votre adresse mail" required>
+                    <input type="number" name="argent"  placeholder="Combien d'argent ajouter ?">
+                    <button type="submit" name="submit">Modifier</button>
+                </form>
+            </section>
         </div>
-        <div class="col-md-9">
-            <input value="<?= $_SESSION["username"] ?>" class="form-control" type="text" name="username">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-2">
-            <label for="">nom</label>
-        </div>
-        <div class="col-md-9">
-            <input value="<?= $_SESSION["nom"] ?>" class="form-control" type="text" name="nom">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-2">
-            <label for="">prenom</label>
-        </div>
-        <div class="col-md-9">
-            <input value="<?= $_SESSION["prenom"]?>" class="form-control" type="text" name="prenom">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-2">
-            <label for="">Adresse Mail</label>
-        </div>
-        <div class="col-md-9">
-            <input value="<?= $_SESSION["mail"] ?>" class="form-control" type="email" name="mail">
-        </div>
-    </div>
-    <br>
-    <input type="submit" class="btn btn-primary">
-</form>
