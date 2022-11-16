85567fcd754f3f97edec23bd2b7621ab38e0d2ba
Aymen Kadri <aymenkadri798@gmail.com>
2022-11-16 19:52:02 +0100

    modification de la navbar

--- navbar.php
+++ navbar.php
@@ -1,16 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="accueil.php">Binamax</a>
-        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Mes paris</a>
-                <a class="nav-link active" aria-current="page" href="profil.php">Mon profil</a>
-                <a class="nav-link active" aria-current="page" href="sedeco.php">se déconnecter</a>
-                <a class>
-            </div>
-        </div>
-    </div>
-</nav>
+<header>
+            <nav class="navbar">
+                <a href="" class="logo">Binamax</a>
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="accueil.php">Accueil</a></li>
+                        <li><a href="#">Mes Paris</a></li>
+                        <li><a href="profil.php"> <?= $_SESSION["username"] ?> (Profil) </a></li>
+                        <?php
+                        if ($_SESSION["administrateur"] == 1) {
+                            echo '<li><a href="staffmenu.php"> Staff Menu </a></li>';
+                        }
+                        ?>
+                        <li><a href="sedeco.php"> Se deconnecter </a></li>
+                    </ul>
+                </div>
+                <a href="" class="solde">Solde : <?= $_SESSION["argent"] ?> €</a>
+            </nav>
+</header>
