201d9d3a936e2c46c5d9687b8f3dd2f004f064fa
Aymen Kadri <aymenkadri5@gmail.com>
2022-11-17 11:47:58 +0100

    navbar fix bug

--- navbar.php
+++ navbar.php
@@ -1,19 +1,19 @@
 <header>
-            <nav class="navbar">
-                <a href="" class="logo">Binamax</a>
-                <div class="nav-links">
-                    <ul>
-                        <li><a href="accueil.php">Accueil</a></li>
-                        <li><a href="#">Mes Paris</a></li>
-                        <li><a href="profil.php"> <?= $_SESSION["username"] ?> (Profil) </a></li>
-                        <?php
-                        if ($_SESSION["administrateur"] == 1) {
-                            echo '<li><a href="staffmenu.php"> Staff Menu </a></li>';
-                        }
-                        ?>
-                        <li><a href="sedeco.php"> Se deconnecter </a></li>
-                    </ul>
-                </div>
-                <a href="profil.php" class="solde">Solde : <?= $_SESSION["argent"] ?> €</a>
-            </nav>
+    <nav class="navbar">
+        <a href="" class="logo">Binamax</a>
+        <div class="nav-links">
+            <ul>
+                <li><a href="accueil.php">Accueil</a></li>
+                <li><a href="mes_paris.php">Mes Paris</a></li>
+                <li><a href="profil.php"> <?= $_SESSION["username"] ?> (Profil) </a></li>
+                <?php
+                if ($_SESSION["administrateur"] == 1) {
+                    echo '<li><a href="staffmenu.php"> Staff Menu </a></li>';
+                }
+                ?>
+                <li><a href="sedeco.php"> Se deconnecter </a></li>
+            </ul>
+        </div>
+        <a href="profil.php" class="solde">Solde : <?= $_SESSION["argent"] ?> €</a>
+    </nav>
 </header>
