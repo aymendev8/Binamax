baf2644ec52feec4ac8c2e263b2c4bade503e63f
aymendev8 <aymenkadri5@gmail.com>
2022-12-04 15:41:53 +0100

    correction de la requete delete : delete du paris dans les_paris et dans les_matchs

--- supprimer_match.php
+++ supprimer_match.php
@@ -1,7 +1,11 @@
 <?php
 require_once("header.php");
 if(isset($_GET['id']) && $_SESSION["administrateur"] == 1 ){
-    $req = $bdd->prepare("DELETE FROM les_matchs WHERE id = :id");
+    $del_lesparis = $bdd->prepare("DELETE FROM les_paris WHERE ID_match = :id");
+    $del_lesparis->execute([
+        "id" => $_GET['id']
+    ]);
+    $req = $bdd->prepare("DELETE FROM les_matchs WHERE ID = :id");
     $req->execute([
         "id" => $_GET['id']
     ]);
